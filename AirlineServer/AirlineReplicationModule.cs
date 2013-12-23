dd9ccf548f2380b7901ffac4c7dc3ff109946ee6
Doron Ben Elazar <b.e.doron@gmail.com>
2013-12-23 23:12:26 +0200

    ArilineReplicaiton
    More data

--- AirlineServer/AirlineReplicationModule.cs
+++ AirlineServer/AirlineReplicationModule.cs
@@ -1,4 +1,5 @@
 ﻿using FlightSearchServer;
+using Org.Apache.Zookeeper.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,9 @@ namespace AirlineServer
     class AirlineReplicationModule : IAirlineReplicationModule
     {
         static ZooKeeper zk = null;
-        static string MachinesNode = "/machines";
-        static string SellersNode = "/sellers";
-        static int secondsTimeout = 10000;
+        static readonly string MACHINES_NODE = "/machines";
+        static readonly string SELLERS_NODE = "/sellers";
+        static readonly int SECONDS_TIMEOUT = 10000;
 
         private string clusterName;
         private string zookeeperAddress;
@@ -33,10 +34,65 @@ namespace AirlineServer
 
             if (null == zk)
             {
-                //zk = new ZooKeeper( address, 
-                //    new TimeSpan(0,0,secondsTimeout), new IWatcher() {
-                    
-                //});
+                zk = new ZooKeeper( address, 
+                    new TimeSpan(0,0,SECONDS_TIMEOUT), new ZooKeeperEvent());
+            }
+
+            checkCluster();
+            checkMachinesNode();
+            checkSellersNode();
+
+        }
+
+        private void checkMachinesNode()
+        {
+            try
+            {
+                Stat s = zk.Exists("/" + clusterName + MACHINES_NODE, false);
+                if (null == s)
+                {
+                    zk.Create("/" + clusterName + MACHINES_NODE, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.Persistent);
+                }
+            }
+            catch (KeeperException e)
+            {
+                Console.WriteLine("Couldn't reach Zookeeper server, " + e.Message);
+                throw e;
+            }
+        }
+
+        private void checkSellersNode()
+        {
+            try
+            {
+                Stat s = zk.Exists("/" + clusterName + MACHINES_NODE, false);
+                if (null == s)
+                {
+                    zk.Create("/" + clusterName + MACHINES_NODE, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.Persistent);
+                }
+            }
+            catch (KeeperException e)
+            {
+                Console.WriteLine("Couldn't reach Zookeeper server, " + e.Message);
+                throw e;
+            }
+        }
+
+
+        private void checkCluster()
+        {
+            try
+            {
+                Stat s = zk.Exists("/" + clusterName, false);
+                if (null == s)
+                {
+                    zk.Create("/" + clusterName, new byte[0], Ids.OPEN_ACL_UNSAFE, CreateMode.Persistent);
+                }
+            }
+            catch (KeeperException e)
+            {
+                Console.WriteLine("Couldn't reach Zookeeper server, " + e.Message);
+                throw e;
             }
         }
 
@@ -45,6 +101,7 @@ namespace AirlineServer
         /// </summary>
         public void initNewMachine()
         {
+            // Create my machine node 
             throw new NotImplementedException();
         }
 
@@ -59,6 +116,12 @@ namespace AirlineServer
         }
 
         public void newMachineJoined()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Dictionary<string, Uri> getSellersAndPrimaries()
         {
             throw new NotImplementedException();
         }
